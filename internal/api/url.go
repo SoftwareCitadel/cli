dc7ec0bfb7e6401f548e9cf0c08b1bc7f82a0745
Alexis Bouchez <80361585+alexisbouchez@users.noreply.github.com>
2024-03-04 18:23:43 +0100

    fix api base url

--- internal/api/url.go
+++ internal/api/url.go
@@ -1,6 +1,6 @@
 package api
 
-var ApiBaseUrl string = "http://127.0.0.1:3333"
+var ApiBaseUrl string = "https://dashboard.softwarecitadel.com"
 
 func RetrieveApiBaseUrl() string {
 	return ApiBaseUrl
