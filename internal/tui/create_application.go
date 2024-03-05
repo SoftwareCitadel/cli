f80fe2ed9d0e1db67d6ae13c677d6931d7bb8f08
Alexis Bouchez <80361585+alexisbouchez@users.noreply.github.com>
2024-03-05 11:21:07 +0100

    fix create application (name too long)

--- internal/tui/create_application.go
+++ internal/tui/create_application.go
@@ -48,119 +48,119 @@ func CreateApplication() string {
 func newChooseComputingSpecs() SelectModel {
 	choices := []SelectChoice{
 		{
-			Name: "shared-cpu-1x 256MB - $3.88/mo",
+			Name: "shared-cpu-1x 256MB",
 			ID:   "shared-cpu-1x-256MB",
 		},
 		{
-			Name: "shared-cpu-1x 512MB - $6.38/mo",
+			Name: "shared-cpu-1x 512MB",
 			ID:   "shared-cpu-1x-512MB",
 		},
 		{
-			Name: "shared-cpu-1x 1GB - $11.4/mo",
+			Name: "shared-cpu-1x 1GB",
 			ID:   "shared-cpu-1x-1GB",
 		},
 		{
-			Name: "shared-cpu-1x 2GB - $21.4/mo",
+			Name: "shared-cpu-1x 2GB",
 			ID:   "shared-cpu-1x-2GB",
 		},
 		{
-			Name: "shared-cpu-2x 512MB - $7.78/mo",
+			Name: "shared-cpu-2x 512MB",
 			ID:   "shared-cpu-2x-512MB",
 		},
 		{
-			Name: "shared-cpu-2x 1GB - $15.28/mo",
+			Name: "shared-cpu-2x 1GB",
 			ID:   "shared-cpu-2x-1GB",
 		},
 		{
-			Name: "shared-cpu-2x 2GB - $25.28/mo",
+			Name: "shared-cpu-2x 2GB",
 			ID:   "shared-cpu-2x-2GB",
 		},
 		{
-			Name: "shared-cpu-2x 4GB - $45.3/mo",
+			Name: "shared-cpu-2x 4GB",
 			ID:   "shared-cpu-2x-4GB",
 		},
 		{
-			Name: "shared-cpu-4x 1GB - $15.56/mo",
+			Name: "shared-cpu-4x 1GB",
 			ID:   "shared-cpu-4x-1GB",
 		},
 		{
-			Name: "shared-cpu-4x 2GB - $33.06/mo",
+			Name: "shared-cpu-4x 2GB",
 			ID:   "shared-cpu-4x-2GB",
 		},
 		{
-			Name: "shared-cpu-4x 4GB - $53.08/mo",
+			Name: "shared-cpu-4x 4GB",
 			ID:   "shared-cpu-4x-4GB",
 		},
 		{
-			Name: "shared-cpu-4x 8GB - $93.1/mo",
+			Name: "shared-cpu-4x 8GB",
 			ID:   "shared-cpu-4x-8GB",
 		},
 		{
-			Name: "shared-cpu-8x 2GB - $31.1/mo",
+			Name: "shared-cpu-8x 2GB",
 			ID:   "shared-cpu-8x-2GB",
 		},
 		{
-			Name: "shared-cpu-8x 4GB - $68.62/mo",
+			Name: "shared-cpu-8x 4GB",
 			ID:   "shared-cpu-8x-4GB",
 		},
 		{
-			Name: "performance-cpu-1x 2GB - $62.0/mo",
+			Name: "performance-cpu-1x 2GB",
 			ID:   "performance-cpu-1x-2GB",
 		},
 		{
-			Name: "performance-cpu-1x 4GB - $82.02/mo",
+			Name: "performance-cpu-1x 4GB",
 			ID:   "performance-cpu-1x-4GB",
 		},
 		{
-			Name: "performance-cpu-1x 8GB - $122.04/mo",
+			Name: "performance-cpu-1x 8GB",
 			ID:   "performance-cpu-1x-8GB",
 		},
 		{
-			Name: "performance-cpu-2x 4GB - $124.0/mo",
+			Name: "performance-cpu-2x 4GB",
 			ID:   "performance-cpu-2x-4GB",
 		},
 		{
-			Name: "performance-cpu-2x 8GB - $184.04/mo",
+			Name: "performance-cpu-2x 8GB",
 			ID:   "performance-cpu-2x-8GB",
 		},
 		{
-			Name: "performance-cpu-2x 16GB - $264.08/mo",
+			Name: "performance-cpu-2x 16GB",
 			ID:   "performance-cpu-2x-16GB",
 		},
 		{
-			Name: "performance-cpu-4x 8GB - $248.0/mo",
+			Name: "performance-cpu-4x 8GB",
 			ID:   "performance-cpu-4x-8GB",
 		},
 		{
-			Name: "performance-cpu-4x 16GB - $388.08/mo",
+			Name: "performance-cpu-4x 16GB",
 			ID:   "performance-cpu-4x-16GB",
 		},
 		{
-			Name: "performance-cpu-4x 32GB - $548.16/mo",
+			Name: "performance-cpu-4x 32GB",
 			ID:   "performance-cpu-4x-32GB",
 		},
 		{
-			Name: "performance-cpu-8x 16GB - $496.0/mo",
+			Name: "performance-cpu-8x 16GB",
 			ID:   "performance-cpu-8x-16GB",
 		},
 		{
-			Name: "performance-cpu-8x 32GB - $796.16/mo",
+			Name: "performance-cpu-8x 32GB",
 			ID:   "performance-cpu-8x-32GB",
 		},
 		{
-			Name: "performance-cpu-8x 64GB - $1116.32/mo",
+			Name: "performance-cpu-8x 64GB",
 			ID:   "performance-cpu-8x-64GB",
 		},
 		{
-			Name: "performance-cpu-16x 32GB - $992.02/mo",
+			Name: "performance-cpu-16x 32GB",
 			ID:   "performance-cpu-16x-32GB",
 		},
 		{
-			Name: "performance-cpu-16x 64GB - $1612.32/mo",
+			Name: "performance-cpu-16x 64GB",
 			ID:   "performance-cpu-16x-64GB",
 		},
 		{
-			Name: "performance-cpu-16x 128GB - $2248.64/mo",
+			Name: "performance-cpu-16x 128GB",
 			ID:   "performance-cpu-16x-128GB",
 		},
 	}
